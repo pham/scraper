57b812c3f1ebf5802c2e7cbe60a33d9897f2cf8b
Paul Pham <paul@aquaron.com>
2020-09-03 07:55:06 -0700

    Update packages to vulnerabilities. Remove newegg/overstock

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,9 +6,7 @@ const urls = [
   'https://www.amazon.com/Bose-Cancelling-Wireless-Bluetooth-Headphones/dp/B07Q9MJKBV/ref=sr_1_2_sspa',
   'https://www.target.com/p/sony-wh1000xm3-b-headphones/-/A-75574683',
   'https://www.walmart.com/ip/Apple-AirPods-with-Charging-Case-Latest-Model/604342441',
-  'https://www.alibaba.com/product-detail/Custom-keyboard-60-Keyboard-GH60-GK64_60839942771.html',
-  'https://www.newegg.com/p/32N-00EB-00006',
-  'https://www.overstock.com/Home-Garden/Pure-Garden-52-inch-Giant-Agave-Floor-Plant/11765296/product.html'
+  'https://www.alibaba.com/product-detail/Custom-keyboard-60-Keyboard-GH60-GK64_60839942771.html'
 ];
 
 urls.forEach( site =>
